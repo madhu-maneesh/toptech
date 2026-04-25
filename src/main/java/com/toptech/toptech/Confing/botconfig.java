e7b4445d2c50e0b843a3c4328deee883df512a45
madhu-maneesh <madhumaneeshshetty1@gmail.com>
2026-04-25 23:51:53 +0530

    added bot registration config

--- src/main/java/com/toptech/toptech/Confing/botconfig.java
+++ src/main/java/com/toptech/toptech/Confing/botconfig.java
@@ -4,21 +4,18 @@ import com.toptech.toptech.Service.botService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
 public class botconfig {
 
     @Bean
-    public TelegramBotsApi telegramBotsApi(botService botService) {
-        try {
-            TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
-            api.registerBot(botService);
-            return api;
-        } catch (Exception e) {
-            System.out.println("Webhook error ignored: " + e.getMessage());
-            return null;
-        }
+    public TelegramBotsApi telegramBotsApi(botService bot) throws Exception {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(bot);
+
+        System.out.println("🔥 BOT REGISTERED SUCCESSFULLY");
+
+        return api;
     }
 }
